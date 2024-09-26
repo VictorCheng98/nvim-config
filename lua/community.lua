cdfb8bf2db73323d5eff58f3de3c22a45c664b9f
Victor Cheng <victorcheng12345@gmail.com>
2024-09-26 12:29:53 -0700

    Fix noice

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,31 @@ return {
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
 
   -- import/override with your plugins folder
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = function(_, opts)
+      local utils = require "astrocore"
+      return utils.extend_tbl(opts, {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+          signature = {
+            enabled = false,
+          },
+          hover = {
+            enabled = false,
+          },
+        },
+      })
+    end,
+  },
 }
