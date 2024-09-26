4107a7e47bfb50a5aeb8876bbe364f8bd6f20bd9
Victor Cheng <victorcheng12345@gmail.com>
2024-09-25 18:37:22 -0700

    Remove noice

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.motion.mini-move" },
 
   -- import/override with your plugins folder
