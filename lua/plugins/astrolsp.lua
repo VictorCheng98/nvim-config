e7d7fddce0c82b7b02ff5dd47160d3796788e4b8
Victor Cheng <victorcheng12345@gmail.com>
2025-07-21 20:16:26 -0400

    Update formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -30,6 +30,8 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
         "ts_ls",
+        "vtsls",
+        "vts_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
